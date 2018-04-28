c3efc3939e505ab128ca3245165ea13e6c1641e2
Jeff Huang <herbage_h2h@sina.com>
2018-04-28 17:17:37 +0800

    update

--- demo-activiti/src/main/java/cc/hw3a/demo/act/BpmnModelGenerator.java
+++ demo-activiti/src/main/java/cc/hw3a/demo/act/BpmnModelGenerator.java
@@ -17,54 +17,54 @@ import org.activiti.bpmn.model.UserTask;
 import org.apache.commons.io.FileUtils;
 
 public class BpmnModelGenerator {
-    public static BpmnModel simple() {
+	public static BpmnModel simple() {
 		BpmnModel model = new BpmnModel();
 
 		Process proc = new Process();
 		proc.setId("simple");
-        proc.setName("simple");
+		proc.setName("simple");
 		model.addProcess(proc);
 
 		StartEvent startEvent = new StartEvent();
 		startEvent.setId("startEvent");
-        startEvent.setName("start");
+		startEvent.setName("start");
 		proc.addFlowElement(startEvent);
 
 		UserTask task1 = new UserTask();
 		task1.setId(UUID.randomUUID().toString());
 		task1.setName("apply");
-        proc.addFlowElement(task1);
+		proc.addFlowElement(task1);
 
 		SequenceFlow flow1 = new SequenceFlow();
 		flow1.setSourceRef(startEvent.getId());
 		flow1.setTargetRef(task1.getId());
-        flow1.setId(UUID.randomUUID().toString());
-        proc.addFlowElement(flow1);
+		flow1.setId(UUID.randomUUID().toString());
+		proc.addFlowElement(flow1);
 
-        EndEvent endEvent = new EndEvent();
-        endEvent.setId("endEvent");
-        endEvent.setName("end");
-        proc.addFlowElement(endEvent);
+		EndEvent endEvent = new EndEvent();
+		endEvent.setId("endEvent");
+		endEvent.setName("end");
+		proc.addFlowElement(endEvent);
 
 		SequenceFlow flow2 = new SequenceFlow();
 		flow2.setSourceRef(task1.getId());
 		flow2.setTargetRef(endEvent.getId());
-        flow2.setId(UUID.randomUUID().toString());
-        proc.addFlowElement(flow2);
+		flow2.setId(UUID.randomUUID().toString());
+		proc.addFlowElement(flow2);
 
-        startEvent.setOutgoingFlows(Arrays.asList(flow1));
-        task1.setOutgoingFlows(Arrays.asList(flow2));
+		startEvent.setOutgoingFlows(Arrays.asList(flow1));
+		task1.setOutgoingFlows(Arrays.asList(flow2));
 
-        return model;
-    }
+		return model;
+	}
 
 	public static void main(String ... args) throws Exception {
-        BpmnModel model = BpmnModelGenerator.simple();
+		BpmnModel model = BpmnModelGenerator.simple();
 
-        byte[] bpmnBytes = new BpmnXMLConverter().convertToXML(model);
-        ByteArrayInputStream bais = new ByteArrayInputStream(bpmnBytes);
-        FileUtils.copyInputStreamToFile(bais, new File("/Users/huanghao/tmp/aa.bpmn20.xml"));
+		byte[] bpmnBytes = new BpmnXMLConverter().convertToXML(model);
+		ByteArrayInputStream bais = new ByteArrayInputStream(bpmnBytes);
+		FileUtils.copyInputStreamToFile(bais, new File("/Users/huanghao/tmp/aa.bpmn20.xml"));
 
-        System.out.println("success");
+		System.out.println("success");
 	}
 }
