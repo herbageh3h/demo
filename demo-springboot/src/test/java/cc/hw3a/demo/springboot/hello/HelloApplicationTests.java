69d81fbe212a36b973ac411ecb0613cce6a4af45
Jeff Huang <herbage_h2h@sina.com>
2019-12-01 17:38:57 +0800

    add spring boot demo

---
+++ demo-springboot/src/test/java/cc/hw3a/demo/springboot/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.hw3a.demo.springboot.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
